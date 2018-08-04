97557afbb309b7701df0e948c8502dbca44f1c17
kimdaeman14 <kimdaeman14@gmail.com>
2018-08-05 02:11:43 +0900

    초기프로젝트생성

---
+++ Dustdust/Dustdust/Dustdust.swift
@@ -0,0 +1,13 @@
+//
+//  Dustdust.swift
+//  Dustdust
+//
+//  Created by kimdaeman14 on 2018. 8. 5..
+//  Copyright © 2018년 GoldenShoe. All rights reserved.
+//
+
+import Foundation
+
+struct Dustdust {
+    <#fields#>
+}
---
+++ Dustdust/Dustdust/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dustdust
+//
+//  Created by kimdaeman14 on 2018. 8. 5..
+//  Copyright © 2018년 GoldenShoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
